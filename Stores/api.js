1692469cc6763e5154a4ba1d18c743ee26790e57
Kitty <zainab_97@hotmail.co.uk>
2021-11-23 20:10:43 +0300

    styling

--- Stores/api.js
+++ Stores/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const baseURL = "http://192.168.8.113:8000"
+// export const baseURL = "http://192.168.8.113:8000"
+export const baseURL = "http://192.168.1.55:8000";
 export const api = axios.create({
-    baseURL: `${baseURL}/api`,
+  baseURL: `${baseURL}/api`,
 });
